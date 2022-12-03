4b4197eb879821c43957903a36de33f73f666ec6
alexpolato <alexandrecpolato@gmail.com>
2022-12-03 10:42:11 -0300

    fix: package.json

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ const bets = require("./controllers/bet");
 const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
+const cors = require("cors");
+
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
-
 app.post("/auth/login", auth.login);
 app.post("/auth/register", auth.register);
 app.post("/bets/bet", bets.create);
