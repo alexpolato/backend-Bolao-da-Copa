e7c4980f3d135432b6a2c512dda9c7d5a3313c61
alexpolato <alexandrecpolato@gmail.com>
2022-12-03 16:21:26 -0300

    fixup update code

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ const auth = require("./controllers/auth");
 const bets = require("./controllers/bet");
 const bodyParser = require("body-parser");
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 const cors = require("cors");
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
-app.post("/auth/login", auth.login);
+app.get("/auth/login", auth.login);
 app.post("/auth/register", auth.register);
 app.post("/bets/bet", bets.create);
 
-app.listen(port, () =>
-  console.log(`Project running at: http://localhost:${port}`)
+app.listen(PORT, () =>
+  console.log(`Project running at: http://localhost:${PORT}`)
 );
