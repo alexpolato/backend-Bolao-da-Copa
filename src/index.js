396b8f713f0c41f44248c9fcb18808c2d2d4f74f
alexpolato <alexandrecpolato@gmail.com>
2022-12-05 13:41:31 -0300

    feat: checking

--- src/index.js
+++ src/index.js
@@ -6,16 +6,17 @@ const bets = require("./controllers/bet");
 const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const cors = require("cors");
 
-app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
+
 app.post("/auth/login", auth.login);
 app.post("/auth/register", auth.register);
 app.post("/bets/bet", bets.create);
 
-res.send("Hello Alex");
+app.get("/", function (req, res) {
+  res.send("You are in!");
+});
 
 app.listen(PORT, () =>
   console.log(`Project running at: http://localhost:${PORT}`)
