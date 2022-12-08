eb5b3352c54c0b25c9ae5d4ec1cd500dcaea5742
alexpolato <alexandrecpolato@gmail.com>
2022-12-08 17:38:07 -0300

    feat: cors

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 const express = require("express");
 // const serverless = require("serverless-http");
-
+const cors = require("cors");
 const auth = require("./controllers/auth");
 const bets = require("./controllers/bet");
 const bodyParser = require("body-parser");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3050;
 
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
-
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.post("/auth/login", auth.login);
 app.post("/auth/register", auth.register);
 app.post("/bets/bet", bets.create);
